bd9c239886d410cf2101c88f4433cee2be0c18fc
Mr.O <oscar@gensolve.com>
2017-10-16 22:10:16 +1000

    Hangfire console

---
+++ BBPRMonitor/hangfire/HangFireConsole.cs
@@ -0,0 +1,49 @@
+﻿using Hangfire.Console;
+using Hangfire.Console.Progress;
+using Hangfire.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBPRMonitor
+{
+    public class HangFireConsole
+    {
+
+        PerformContext _context;
+        IProgressBar _progressBar;
+
+        public enum ActivityType { Info, Warning, Exception }
+
+        public HangFireConsole(PerformContext context, IProgressBar progressBar)
+        { this._context = context; this._progressBar = progressBar; }
+
+        public void Progress(int value)
+        { try { if (this._progressBar != null) { this._progressBar.SetValue(value); } } catch { } }
+
+        public void Write(string logmsg, ActivityType activityType, Exception exception = null)
+        {
+            try
+            {
+                if (_context != null)
+                {
+                    StringBuilder sbBody = new StringBuilder(logmsg); sbBody.AppendLine();
+                    try { if (exception != null) { sbBody.AppendLine(exception.Message); sbBody.AppendLine(exception.StackTrace.ToString()); sbBody.AppendLine(); } } catch { }
+                    string dt = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+                    switch (activityType)
+                    {
+                        case ActivityType.Info: try { _context.SetTextColor(ConsoleTextColor.White); } catch { } break;
+                        case ActivityType.Warning: try { _context.SetTextColor(ConsoleTextColor.Yellow); } catch { } break;
+                        case ActivityType.Exception: try { _context.SetTextColor(ConsoleTextColor.Red); } catch { } break;
+                        default: try { _context.SetTextColor(ConsoleTextColor.Blue); } catch { } break;
+                    }
+                    try { _context.WriteLine(dt + ":: " + sbBody.ToString()); _context.ResetTextColor(); } catch { }
+                }
+            }
+            catch { }
+        }
+
+    }
+}
--- BBPRMonitor/jobs/PullRequestQueueMonitor.cs
+++ BBPRMonitor/jobs/PullRequestQueueMonitor.cs
@@ -1,17 +1,33 @@
 ﻿using Hangfire;
+using Hangfire.Console;
 using Hangfire.Server;
+using NumatoRelayHelper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BBPRMonitor
 {
     public class PullRequestQueueMonitor
     {
+        IRelayHelper _relayHelper;
+
+
         [DisableConcurrentExecution(60)]
         [AutomaticRetry(Attempts = 0, OnAttemptsExceeded = AttemptsExceededAction.Delete)]
-        public void ExecuteAction(PerformContext context) { }
+        public void ExecuteAction(PerformContext context) {
+            var console = new HangFireConsole(context, context.WriteProgressBar());
+            console.Write("Initializing PullRequestQueueMonitor Service ...", HangFireConsole.ActivityType.Info);
+
+
+            (new Thread(() => {
+                _relayHelper = Program.container.Resolve<IRelayHelper>();
+                _relayHelper.Alarm();
+            })).Start();
+
+        }
     }
 }
