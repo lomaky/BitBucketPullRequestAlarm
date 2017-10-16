084f4e3e2c95686f4e191cb6204640f83191d95d
Mr.O <oscar@gensolve.com>
2017-10-16 22:33:14 +1000

    Process Queue

--- BBPRMonitor/jobs/PullRequestQueueMonitor.cs
+++ BBPRMonitor/jobs/PullRequestQueueMonitor.cs
@@ -1,9 +1,12 @@
-﻿using Hangfire;
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using Hangfire;
 using Hangfire.Console;
 using Hangfire.Server;
 using NumatoRelayHelper;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -15,6 +18,10 @@ namespace BBPRMonitor
     {
         IRelayHelper _relayHelper;
 
+        public string AccessKeyId { get { return ConfigurationManager.AppSettings["AccessKeyId"]; } }
+        public string SecretKey { get { return ConfigurationManager.AppSettings["SecretKey"];  } }
+        public string SQSUrl { get { return ConfigurationManager.AppSettings["SQSUrl"]; } }
+
 
         [DisableConcurrentExecution(60)]
         [AutomaticRetry(Attempts = 0, OnAttemptsExceeded = AttemptsExceededAction.Delete)]
@@ -22,11 +29,35 @@ namespace BBPRMonitor
             var console = new HangFireConsole(context, context.WriteProgressBar());
             console.Write("Initializing PullRequestQueueMonitor Service ...", HangFireConsole.ActivityType.Info);
 
+            var _client = new AmazonSQSClient(
+                  awsAccessKeyId: AccessKeyId,
+                  awsSecretAccessKey: SecretKey,
+                  region: Amazon.RegionEndpoint.USEast1);
 
-            (new Thread(() => {
-                _relayHelper = Program.container.Resolve<IRelayHelper>();
-                _relayHelper.Alarm();
-            })).Start();
+            var _request = new ReceiveMessageRequest
+            { QueueUrl = SQSUrl, MaxNumberOfMessages = 10 };
+
+            console.Write("Querying Queue " + SQSUrl + "...", HangFireConsole.ActivityType.Info);
+
+            var _response = _client.ReceiveMessage(_request);
+
+            if (_response.Messages.Count > 0)
+            {
+                console.Write(_response.Messages.Count + " PullRequest(s) Found", HangFireConsole.ActivityType.Info);
+
+                (new Thread(() =>
+                {
+                    _relayHelper = Program.container.Resolve<IRelayHelper>();
+                    _relayHelper.Alarm();
+                })).Start();
+
+                _client.PurgeQueue(new PurgeQueueRequest { QueueUrl = SQSUrl });
+
+                console.Write("Purging Queue", HangFireConsole.ActivityType.Info);
+
+            }
+
+            console.Write("Finalizing PullRequestQueueMonitor Service.", HangFireConsole.ActivityType.Info);
 
         }
     }
